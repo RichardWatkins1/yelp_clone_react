f52076b6a466a0b78a95d0f7de920fb17d81a1d8
RichardWatkins1 <richardwatkins1@gmail.com>
2016-07-19 16:26:55 +0100

    creating markers on our map

--- src/views/main/Container.js
+++ src/views/main/Container.js
@@ -22,6 +22,17 @@ export class Container extends React.Component {
 })
   }
   render() {
+    let children = null;
+    if (this.props.children) {
+      // We have children in the Container component
+      children = React.cloneElement(
+        this.props.children,
+        {
+          google: this.props.google,
+          places: this.state.places,
+          loaded: this.props.loaded
+        });
+    }
     return (
       <div>
         <Map
@@ -30,9 +41,9 @@ export class Container extends React.Component {
           <Header />
           <Sidebar />
           <div className={styles.content}>
-            {/* Setting children routes to be rendered*/}
-            {this.props.children}
-          </div>
+        {/* Setting children routes to be rendered*/}
+        {children}
+      </div>
         </Map>
       </div>
     )
